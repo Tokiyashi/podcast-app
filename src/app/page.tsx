68c94b0eb7153fbb5e4a912156e9b0ff54526fdd
Nikita Rogovskiy <rogich6319@gmail.com>
2023-10-05 19:18:07 +0300

    revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 export default async function Home() {
   return (
-    <main>
+    <div>
       <Link href="/browse">Start browsing music!</Link>
-    </main>
+    </div>
   )
 }
