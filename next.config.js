10d74f501186ff31d1e2c6f4ac6b97c8ec030bb1
Nikita Rogovskiy <rogich6319@gmail.com>
2023-10-18 20:38:51 +0300

    Hotfix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
-}
+  experimental: {
+    appDir: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
